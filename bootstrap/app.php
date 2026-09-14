e7ccd81dbbe39b6fd54cccb73bdec97a7f810e86
Rynn <nakapediaa@gmail.com>
2026-09-14 12:37:41 +0700

    fix: restore namespace backslashes for Application in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,8 @@
 <?php
 
-use IlluminateFoundationApplication;
-use IlluminateFoundationConfigurationExceptions;
-use IlluminateFoundationConfigurationMiddleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
